6ffd9c447c16a0eb0bd58837c37261ce44a6bb12
Vasiliy Yaklushin <3a3a3a3@gmail.com>
2011-06-30 16:34:34 +0600

    * forms rework. It is more convenient to extend them now

--- application/modules/user/forms/Login.php
+++ application/modules/user/forms/Login.php
@@ -1,37 +1,86 @@
 <?php
-
+/**
+ * Login form
+ */
 class User_Form_Login extends Zend_Form
 {
     public function init()
     {
         $this->setName("login");
         $this->setMethod('post');
-
-        $this->addElement('text', 'username', array(
+        $this->_addElements();
+    }
+ 
+    /**
+     * @return User_Form_Login
+     */
+    protected function _addElements()
+    {
+        $this->addElements(array(
+            $this->_getFieldUsername(),
+            $this->_getFieldPassword(),
+            $this->_getFieldRemember(),
+            $this->_getFieldSubmit(),
+        ));
+        return $this;
+    }    
+    
+    /**
+     * @return Zend_Form_Element_Text
+     */        
+    protected function _getFieldUsername()
+    {
+        $field = $this->createElement('text', 'username', array(
             'filters'     => array('StringTrim'),
             'validators'  => array(),
             'required'    => true,
             'label'       => 'Имя:',
         ));
-
-        $this->addElement('password', 'password', array(
+        
+        return $field;
+    }    
+    
+    /**
+     * @return Zend_Form_Element_Password
+     */        
+    protected function _getFieldPassword()
+    {
+        $field = $this->createElement('password', 'password', array(
             'filters'     => array(),
             'validators'  => array(),
             'required'    => true,
             'label'       => 'Пароль:',
         ));
+        
+        return $field;
+    }    
 
-        $this->addElement('checkbox', 'remember', array(
+    /**
+     * @return Zend_Form_Element_Checkbox
+     */        
+    protected function _getFieldRemember()
+    {
+        $field = $this->createElement('checkbox', 'remember', array(
             'filters'     => array(),
             'validators'  => array(),
             'required'    => false,
             'label'       => 'Запомнить',
         ));
+        
+        return $field;
+    }    
 
-        $this->addElement('submit', 'submit', array(
+    /**
+     * @return Zend_Form_Element_Submit
+     */        
+    protected function _getFieldSubmit()
+    {
+        $field = $this->createElement('submit', 'submit', array(
             'required' => false,
             'ignore'   => true,
             'label'    => 'Войти',
         ));
-    }
+        
+        return $field;
+    }    
 }
--- application/modules/user/forms/Register.php
+++ application/modules/user/forms/Register.php
@@ -1,37 +1,87 @@
 <?php
-
+/**
+ * Register form
+ */
 class User_Form_Register extends Zend_Form
 {
     public function init()
     {
         $this->setName("register");
         $this->setMethod('post');
-
-        $this->addElement('text', 'username', array(
+        $this->_addElements();
+    }
+ 
+    /**
+     * @return User_Form_Register
+     */
+    protected function _addElements()
+    {
+        $this->addElements(array(
+            $this->_getFieldUsername(),
+            $this->_getFieldPassword(),
+            $this->_getFieldEmail(),
+            $this->_getFieldSubmit(),
+        ));
+        return $this;
+    }    
+    
+    /**
+     * @return Zend_Form_Element_Text
+     */        
+    protected function _getFieldUsername()
+    {
+        $field = $this->createElement('text', 'username', array(
             'filters'     => array('StringTrim'),
             'validators'  => array(),
             'required'    => true,
             'label'       => 'Имя:',
         ));
-
-        $this->addElement('password', 'password', array(
+        
+        return $field;
+    }    
+    
+    /**
+     * @return Zend_Form_Element_Password
+     */        
+    protected function _getFieldPassword()
+    {
+        $field = $this->createElement('password', 'password', array(
             'filters'     => array(),
             'validators'  => array(),
             'required'    => true,
             'label'       => 'Пароль:',
         ));
+        
+        return $field;
+    }    
 
-        $this->addElement('text', 'email', array(
+    /**
+     * @return Zend_Form_Element_Text
+     */        
+    protected function _getFieldEmail()
+    {
+        $field = $this->createElement('text', 'email', array(
             'filters'     => array('StringTrim'),
             'validators'  => array('EmailAddress'),
             'required'    => true,
             'label'       => 'Email:',
         ));
+        
+        return $field;
+    }    
 
-        $this->addElement('submit', 'submit', array(
+
+    /**
+     * @return Zend_Form_Element_Submit
+     */        
+    protected function _getFieldSubmit()
+    {
+        $field = $this->createElement('submit', 'submit', array(
             'required' => false,
             'ignore'   => true,
             'label'    => 'Добавить пользователя',
         ));
-    }
+        
+        return $field;
+    }    
 }
